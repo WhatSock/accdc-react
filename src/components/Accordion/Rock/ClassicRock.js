ccf71c716304cc691b33028647844330e8a5fd46
Laurence Lewis <laurence.r.lewis@gmail.com>
2019-03-08 17:21:04 +1000

    Fixed missing images causing RunWebServer.bat to error.
    Missing images for Missing images found in 'https://github.com/WhatSock/bootstrap' Images copied to 'bootstrap-react\src\img\accordion' and 'AlternativeRock.js ,  Classical.js and  ClassicRock.js updated to map to images and fixed other minor closing paths bugs.

--- src/components/Accordion/Rock/ClassicRock.js
+++ src/components/Accordion/Rock/ClassicRock.js
@@ -3,24 +3,24 @@ import React from "react";
 class ClassicRock extends React.Component {
   render() {
     return (
-      <div id="accordion-sect3" className="content-panel">
+      <div id="accordion-sect2" className="content-panel">
         <p>
         <img
-          src="../../../img/music/r_gnr_uyi.jpg"
+          src={require('../../../img/accordion/r_gnr_uyi.jpg')} 
           alt="Guns N' Roses - Use Your Illusion 1"
           title="Guns N' Roses - Use Your Illusion 1"
         />
         </p>
         <p>
         <img
-          src="../../../img/music/r_pj_t.jpg"
+          src={require('../../../img/accordion/r_pj_t.jpg')} 
           alt="Pearl Jam - Ten"
           title="Pearl Jam - Ten"
         />
         </p>
         <p>
         <img
-          src="../../../img/music/r_pf_dst.jpg"
+          src={require('../../../img/accordion/r_pf_dst.jpg')} 
           alt="Pink Floyd - Delicate Sound of Thunder"
           title="Pink Floyd - Delicate Sound of Thunder"
         />
@@ -31,3 +31,4 @@ class ClassicRock extends React.Component {
 }
 
 export default ClassicRock;
+
